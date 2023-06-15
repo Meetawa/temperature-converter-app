c83432f5cd390fcab71cc3fedecda26ca62d3e88
RAHUL <rahulmeetawa@gmail.com>
2023-06-15 16:57:31 +0530

    add

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,25 +1,13 @@
 import React from "react";
-import BoilingVerdict from "./BoilingVerdict";
+import { TemperatureInput } from "./TemperatureInput";
 
 class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = { temperature: "" };
-  }
-
-  handleChange(e) {
-    this.setState({ temperature: e.target.value });
-  }
-
   render() {
-    const temperature = this.state.temperature;
     return (
-      <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
-        <input value={temperature} onChange={this.handleChange} />
-        <BoilingVerdict celsius={parseFloat(temperature)} />
-      </fieldset>
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
     );
   }
 }
